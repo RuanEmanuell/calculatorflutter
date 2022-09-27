af73fd4cbd77bf6440969b49615c4b8a571ed2ad
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-27 20:54:23 -0300

    Finished the number buttons

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import "../partials/numberbutton.dart";
-
 
 class HomeScreen extends StatefulWidget{
   @override
@@ -9,13 +7,19 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreen extends State<HomeScreen>{
 
-
+  var number="";
   var firstNumber=[];
+  var style=TextStyle(
+    fontSize:30,
+    color:Colors.white
+  );
   
   @override
   Widget build(BuildContext context){
   var screenWidth=MediaQuery.of(context).size.width;
   var screenHeight=MediaQuery.of(context).size.height;
+  var screenWidthGlobal=screenWidth/5;
+  var screenHeightGlobal=screenHeight/7;
   return Scaffold(
     body: Center(
       child:Container(
@@ -23,7 +27,6 @@ class _HomeScreen extends State<HomeScreen>{
         child: Column(
           children:[
             Container(
-              padding:EdgeInsets.only(top: 50),
               margin:EdgeInsets.only(top: 50),
               height:screenHeight/4,
               width:screenWidth,
@@ -39,36 +42,211 @@ class _HomeScreen extends State<HomeScreen>{
               color:Colors.blue,
               child:Column(
                 children: [
-                  Row(
-                    children: [
-                  NumberButton(number:"1", firstNumber: firstNumber,),
-                  NumberButton(number:"2", firstNumber: firstNumber,),
-                  NumberButton(number:"3", firstNumber: firstNumber,),
-                  ]),
-                   Row(
-                    children: [
-                  NumberButton(number:"4", firstNumber: firstNumber),
-                  NumberButton(number:"5", firstNumber: firstNumber,),
-                  NumberButton(number:"6", firstNumber: firstNumber,),
-                  ]),
-                   Row(
-                    children: [
-                  NumberButton(number:"7", firstNumber: firstNumber,),
-                  NumberButton(number:"8", firstNumber: firstNumber,),
-                  NumberButton(number:"9", firstNumber: firstNumber,),
-                  ]),
-                   Row(
-                    children: [
-                  NumberButton(number:"0", firstNumber: firstNumber,),
-                  ]),
+                 Row(
+                  children: [
+                   Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("1", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="1";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                     Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("2", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="2";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                    Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("3", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="3";
+                          firstNumber.add(number);
+                        });
+                        }
+                    ),
+                    )
+                ]
+              ),
+              Row(
+                  children: [
+                   Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("4", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="4";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                     Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("5", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="5";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                    Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("6", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="6";
+                          firstNumber.add(number);
+                        });
+                        }
+                    ),
+                    )
+                ]
+              ),
+              Row(
+                  children: [
+                   Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("7", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="7";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                     Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("8", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="8";
+                          firstNumber.add(number);
+                        });
+                        }
+                    )
+                    ),
+                    Container(
+                    margin:EdgeInsets.all(5),
+                    width:screenWidthGlobal,
+                      height:screenHeightGlobal,
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      color:Color.fromARGB(115, 0, 0, 0),
+                      ),
+                      child:TextButton(
+                      child:Text("9", style:style),
+                        onPressed:(){   
+                          setState((){
+                          number="9";
+                          firstNumber.add(number);
+                        });
+                        }
+                    ),
+                    )
+                ]
+              ),
+              Row(
+                children: [
+                  Container(
+                        margin:EdgeInsets.all(5),
+                        width:screenWidthGlobal,
+                          height:screenHeightGlobal,
+                          decoration:BoxDecoration(
+                          borderRadius:BorderRadius.circular(20),
+                          color:Color.fromARGB(115, 0, 0, 0),
+                          ),
+                          child:TextButton(
+                          child:Text("0", style:style),
+                            onPressed:(){   
+                              setState((){
+                              number="0";
+                              firstNumber.add(number);
+                            });
+                            }
+                        ),
+                        ),
                 ],
               )
-            )
+              
           ]
-        ),
+        )
       )
-      )
-    );
+      ]
+    )
+    )
+    )
+  );
   }
 
 }
