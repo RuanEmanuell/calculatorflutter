a41f2b7bd157dff1ac3544b67e65e4a861c4b57c
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-27 19:41:49 -0300

    Added buttons

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "../partials/numberbutton.dart";
 
 
 class HomeScreen extends StatefulWidget{
@@ -6,9 +7,10 @@ class HomeScreen extends StatefulWidget{
   _HomeScreen createState()=> _HomeScreen();
 }
 
-
-
 class _HomeScreen extends State<HomeScreen>{
+
+
+  var firstNumber=[];
   
   @override
   Widget build(BuildContext context){
@@ -17,19 +19,53 @@ class _HomeScreen extends State<HomeScreen>{
   return Scaffold(
     body: Center(
       child:Container(
-        width: screenWidth,
-        height:screenHeight/3,
-        child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4
-              ),
-              itemCount:10,
-              itemBuilder:((context, index) {
-                return Container(
-                  child:Text(index.toString())
-                );
-              })
+        color:Colors.red,
+        child: Column(
+          children:[
+            Container(
+              padding:EdgeInsets.only(top: 50),
+              margin:EdgeInsets.only(top: 50),
+              height:screenHeight/4,
+              width:screenWidth,
+              color:Colors.red,
+              child:Text(firstNumber.toString(), style:TextStyle(
+                color:Colors.black,
+                fontSize:50
+              ))
             ),
+            Container(
+              height:screenHeight/1.334-50,
+              width:screenWidth,
+              color:Colors.blue,
+              child:Column(
+                children: [
+                  Row(
+                    children: [
+                  NumberButton(number:"1", firstNumber: firstNumber,),
+                  NumberButton(number:"2", firstNumber: firstNumber,),
+                  NumberButton(number:"3", firstNumber: firstNumber,),
+                  ]),
+                   Row(
+                    children: [
+                  NumberButton(number:"4", firstNumber: firstNumber),
+                  NumberButton(number:"5", firstNumber: firstNumber,),
+                  NumberButton(number:"6", firstNumber: firstNumber,),
+                  ]),
+                   Row(
+                    children: [
+                  NumberButton(number:"7", firstNumber: firstNumber,),
+                  NumberButton(number:"8", firstNumber: firstNumber,),
+                  NumberButton(number:"9", firstNumber: firstNumber,),
+                  ]),
+                   Row(
+                    children: [
+                  NumberButton(number:"0", firstNumber: firstNumber,),
+                  ]),
+                ],
+              )
+            )
+          ]
+        ),
       )
       )
     );
