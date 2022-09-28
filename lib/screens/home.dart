054e1340de2657fd66095958d0b4652ba284d1dd
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-28 19:29:59 -0300

    Adicionado os números do resultado na tela

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,8 +12,7 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreen extends State<HomeScreen>{
 
-  TextEditingController firstController=TextEditingController();
-  TextEditingController secondController=TextEditingController();
+  TextEditingController controller=TextEditingController();
 
   var acertos=0;
   var resultado=[];
@@ -36,16 +35,69 @@ class _HomeScreen extends State<HomeScreen>{
 
   @override
   Widget build(BuildContext context){
+    var statusBarHeight=MediaQuery.of(context).viewPadding.top;
     return Scaffold(
       body: Center(
         child: Column(
           children: [
-              Text(data["dezenas"].toString())
+              Container(
+                child:Column(
+                      children:[
+                        Container(
+                        margin:EdgeInsets.only(
+                        top: statusBarHeight+10),
+                        child: Text("Resultado do concurso ${data["numero_concurso"]}",
+                        style:TextStyle(
+                          fontSize:25
+                          )
+                        )),
+                        GridView.count(
+                              crossAxisCount:5,
+                              shrinkWrap: true,
+                            children:List.generate(data["dezenas"].length, (index) { 
+                              return Container(
+                                      margin:EdgeInsets.all(8),
+                                      padding:EdgeInsets.all(8),
+                                      decoration:BoxDecoration(
+                                        color:Color.fromARGB(255, 75, 141, 76),
+                                        border:Border.all(color:Colors.green,
+                                        width:5),
+                                        borderRadius:BorderRadius.circular(20)
+                                      ),
+                                      child:Text(data["dezenas"][index], style:TextStyle(
+                                        fontSize:30,
+                                        color:Colors.white
+                                      ))
+                                  );
+                                }
+                              )
+                            )
+                          ]
+                         )
+                      ),
+                      Container(
+                        margin:EdgeInsets.only(top: 20),
+                        child:Column(
+                          children:[
+                            Text("Checar seus acertos"),
+                            TextField(
+                              controller:controller
+                            ),
+                            ElevatedButton(
+                              onPressed:(){
 
-            ]
-        )
-        ),
-      );
+                              },
+                              child:Text("Checar")
+                            ),
+                            Text("Você acertou $acertos números")
+                          
+                          ]
+                       )
+                     )
+                   ]
+                  )
+               ),
+        );
   }
 
 
