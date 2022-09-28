71335a05eb9cfb0ff561d7e796a361527b23abd4
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-28 09:59:41 -0300

    Changed the keyboard type and added input

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,249 +5,94 @@ class HomeScreen extends StatefulWidget{
   _HomeScreen createState()=> _HomeScreen();
 }
 
+
 class _HomeScreen extends State<HomeScreen>{
 
-  var number="";
-  var firstNumber=[];
-  var style=TextStyle(
-    fontSize:30,
-    color:Colors.white
-  );
-  
+  TextEditingController firstController=TextEditingController();
+  TextEditingController secondController=TextEditingController();
+
+  var acertos=0;
+  var resultado=[];
+  var numeros=[];
+
   @override
   Widget build(BuildContext context){
-  var screenWidth=MediaQuery.of(context).size.width;
-  var screenHeight=MediaQuery.of(context).size.height;
-  var screenWidthGlobal=screenWidth/5;
-  var screenHeightGlobal=screenHeight/7;
-  return Scaffold(
-    body: Center(
-      child:Container(
-        color:Colors.red,
+    return Scaffold(
+      body: Center(
         child: Column(
-          children:[
+          children: [
             Container(
-              margin:EdgeInsets.only(top: 50),
-              height:screenHeight/4,
-              width:screenWidth,
-              color:Colors.red,
-              child:Text(firstNumber.toString(), style:TextStyle(
-                color:Colors.black,
-                fontSize:50
-              ))
-            ),
-            Container(
-              height:screenHeight/1.334-50,
-              width:screenWidth,
-              color:Colors.blue,
-              child:Column(
-                children: [
-                 Row(
-                  children: [
-                   Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
+                  margin:EdgeInsets.only(top: 30),
+                  width:300,
+                  height:100,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller:firstController,
+                        keyboardType:TextInputType.number,
+                        decoration:InputDecoration(
+                          hintText:"Resultado do jogo"
+                        )
                       ),
-                      child:TextButton(
-                      child:Text("1", style:style),
-                        onPressed:(){   
+                      ElevatedButton(
+                        onPressed:(){
                           setState((){
-                          number="1";
-                          firstNumber.add(number);
-                        });
-                        }
-                    )
-                    ),
-                     Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
+                           resultado=[];
+                           resultado.add(firstController.text);
+                           print(resultado);
+                          });
+                        },
+                        child:Text("Enviar")
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width:300,
+                  height:100,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller:secondController,
+                        keyboardType:TextInputType.number,
+                        decoration:InputDecoration(
+                          hintText:"Digite uma coluna de números"
+                        )
                       ),
-                      child:TextButton(
-                      child:Text("2", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="2";
-                          firstNumber.add(number);
-                        });
+                      ElevatedButton(
+                        onPressed:(){
+                          setState(() {
+                           numeros=[];
+                           numeros.add(secondController.text);
+                           print(numeros);
+                          });
+                        },
+                        child:Text("Enviar")
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child:Column(
+                    children: [
+                      Container(height: 50),
+                      ElevatedButton(
+                        child:Text("Checar"),
+                        onPressed:(){
+
                         }
-                    )
-                    ),
-                    Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
                       ),
-                      child:TextButton(
-                      child:Text("3", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="3";
-                          firstNumber.add(number);
-                        });
-                        }
-                    ),
-                    )
-                ]
-              ),
-              Row(
-                  children: [
-                   Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("4", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="4";
-                          firstNumber.add(number);
-                        });
-                        }
-                    )
-                    ),
-                     Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("5", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="5";
-                          firstNumber.add(number);
-                        });
-                        }
-                    )
-                    ),
-                    Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("6", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="6";
-                          firstNumber.add(number);
-                        });
-                        }
-                    ),
-                    )
-                ]
-              ),
-              Row(
-                  children: [
-                   Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("7", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="7";
-                          firstNumber.add(number);
-                        });
-                        }
-                    )
-                    ),
-                     Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("8", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="8";
-                          firstNumber.add(number);
-                        });
-                        }
-                    )
-                    ),
-                    Container(
-                    margin:EdgeInsets.all(5),
-                    width:screenWidthGlobal,
-                      height:screenHeightGlobal,
-                      decoration:BoxDecoration(
-                      borderRadius:BorderRadius.circular(20),
-                      color:Color.fromARGB(115, 0, 0, 0),
-                      ),
-                      child:TextButton(
-                      child:Text("9", style:style),
-                        onPressed:(){   
-                          setState((){
-                          number="9";
-                          firstNumber.add(number);
-                        });
-                        }
-                    ),
-                    )
-                ]
-              ),
-              Row(
-                children: [
-                  Container(
-                        margin:EdgeInsets.all(5),
-                        width:screenWidthGlobal,
-                          height:screenHeightGlobal,
-                          decoration:BoxDecoration(
-                          borderRadius:BorderRadius.circular(20),
-                          color:Color.fromARGB(115, 0, 0, 0),
-                          ),
-                          child:TextButton(
-                          child:Text("0", style:style),
-                            onPressed:(){   
-                              setState((){
-                              number="0";
-                              firstNumber.add(number);
-                            });
-                            }
-                        ),
-                        ),
-                ],
-              )
-              
-          ]
-        )
+                      Text("Voce acertou ${acertos} números"),
+                    ],
+                  )
+                )
+          ],
+        ),
       )
-      ]
-    )
-    )
-    )
-  );
+    );
   }
+
+
 
 }
 
